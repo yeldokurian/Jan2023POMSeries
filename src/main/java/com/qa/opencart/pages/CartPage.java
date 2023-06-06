af90ebca23b928b56ed796df33b88483806da692
yeldokurian <yeldokurian67@hotmail.com>
2023-06-06 17:47:08 -0400

    added comments as per reviewer

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,5 +1,12 @@
 package com.qa.opencart.pages;
 
-public class CartPage {
+import org.openqa.selenium.By;
 
+public class CartPage {
+	
+	String name = "cart";
+	By cart = By.id("cart");
+	public void addCart(){
+		System.out.println("cart is added");
+	}
 }
